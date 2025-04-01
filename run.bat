207226358699e3d3fd7d0102e12f724af9738676
Carlosdve20 <carlosdve2.0@gmail.com>
2025-04-01 22:48:36 +0200

    cambio en las clases y agregado tallerdao

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-javac -d bin -cp .:lib/mysql-connector-j-9.2.0.jar ./src/dao/*.java ./src/model/*.java ./src/*.java
+javac -d bin -cp .:lib/mysql-connector-j-9.2.0.jar ./src/dao/*.java ./src/model/*.java ./src/*.javagit 
 java -cp "./bin;.:lib/mysql-connector-j-9.2.0.jar" Main
