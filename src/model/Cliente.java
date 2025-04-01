f1e5250f07ff0a50fc91434aad36642fb8af49e5
Joss33 <josergv.16@gmail.com>
2025-04-01 13:59:26 +0200

    cambio cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,100 +1,84 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Cliente {
-    private int id;
-    private String nombre;
-    private String apellido;
-    private String dni;
-    private String telefono;
-    private String correo;
-    private String direccion;
-    private List<Vehiculo> Vehiculo; 
-
-    public Cliente() {
-        this.Vehiculo = new ArrayList<>();
-    }
-
-    public Cliente(String nombre, String apellido, String dni, String telefono, String correo, String direccion) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.direccion = direccion;
-        this.Vehiculo = new ArrayList<>();
-    }
-
-    
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public List<Vehiculo> getVehiculos() {
-        return Vehiculo;
-    }
-
-    public void agregarVehiculo(Vehiculo vehiculo) {
-        this.Vehiculo.add(vehiculo);
-    }
+private int id;
+private String nombre;
+private String apellido;
+private String dni;
+private String telefono;
+private String correo;
+private String direccon;
+public Cliente() {
 	
-	@Override
-    public String toString() {
-        return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", telefono=" + telefono + ", correo=" + correo + ", direccion=" + direccion + ", vehiculos=" + vehiculos + "]";
-    }
+}
+
+public Cliente(String nombre, String apellido, String dni, String telefono, String correo, String direccon) {
+	
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.dni = dni;
+	this.telefono = telefono;
+	this.correo = correo;
+	this.direccon = direccon;
+}
+
+public Cliente(int id, String nombre, String apellido, String dni, String telefono, String correo, String direccon) {
+	
+	this.id = id;
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.dni = dni;
+	this.telefono = telefono;
+	this.correo = correo;
+	this.direccon = direccon;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+public String getDni() {
+	return dni;
+}
+public void setDni(String dni) {
+	this.dni = dni;
+}
+public String getTelefono() {
+	return telefono;
+}
+public void setTelefono(String telefono) {
+	this.telefono = telefono;
+}
+public String getCorreo() {
+	return correo;
+}
+public void setCorreo(String correo) {
+	this.correo = correo;
+}
+public String getDireccon() {
+	return direccon;
+}
+public void setDireccon(String direccon) {
+	this.direccon = direccon;
+}
+@Override
+public String toString() {
+	return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", telefono="
+			+ telefono + ", correo=" + correo + ", direccon=" + direccon + "]";
+}
+
+
 }
