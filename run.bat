c219d48eabd0f4065f5a8718f2dcda016a6c9870
Carlosdve20 <carlosdve2.0@gmail.com>
2025-04-04 12:27:45 +0200

    run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-javac -d bin -cp .:lib/mysql-connector-j-9.2.0.jar ./src/dao/*.java ./src/model/*.java ./src/*.java
+javac -d bin -cp .:lib/mysql-connector-j-9.2.0.jar ./src/dao/*.java ./src/view/*.java ./src/model/*.java ./src/*.java
 java -cp ".;./bin;lib/mysql-connector-j-9.2.0.jar" Main
